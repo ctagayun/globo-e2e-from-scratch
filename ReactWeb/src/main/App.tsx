adbf03b2b10231cf7d9ea34af1ebe563b6a6da7e
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-10-22 08:36:41 -0500

    Initial Commit

---
+++ ReactWeb/src/main/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+import Header from './Header'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    //container is a bootstrap class that will serve as container
+    //for the rest of the layout
+    <div className="container">
+      <Header subtitle="Providing houses all over the world"/>
+
+    </div>
+   )
+}
+
+export default App
